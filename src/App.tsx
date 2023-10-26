c109db6d2d24730cd0ffc66076ed2e42c41f011b
Tacianojr07 <t.juniior13@gmail.com>
2023-10-26 20:39:18 -0300

    :sparkles: feat:tasks are being rendered

--- src/App.tsx
+++ src/App.tsx
@@ -16,22 +16,22 @@ interface taskProps {
 
 function App() {
   const [descriptionTask, setDescriptionTask] = useState("");
-  const [task, setTask] = useState<taskProps[]>([]);
+  const [tasks, setTasks] = useState<taskProps[]>([]);
 
   useEffect(() => {
     const taskRef = collection(db, "taskUsers");
     const queryRef = query(taskRef, orderBy("created", "asc"));
 
     const getData = onSnapshot(queryRef, (snapshot) => {
-      const tasks = [] as taskProps[];
+      const tasksPush = [] as taskProps[];
 
       snapshot.forEach((doc) => {
-        tasks.push({
+        tasksPush.push({
           id: doc.id,
           task: doc.data().task,
         });
       });
-      setTask(tasks);
+      setTasks(tasksPush);
     });
 
     return () => {
@@ -80,17 +80,22 @@ function App() {
         </button>
       </form>
 
-      <main className="flex flex-col w-full items-center">
-        <section className=" flex flex-col w-full items-center justify-center">
-          <div className="flex justify-between w-full max-w-xl bg-slate-950 py-2 px-2 rounded text-white mb-5">
-            <p>this a task</p>
+      {tasks.map((taskList) => (
+        <main className="flex flex-col w-full items-center">
+          <section className=" flex flex-col w-full items-center justify-center">
+            <div
+              key={taskList.id}
+              className="flex justify-between w-full max-w-xl bg-slate-950 py-2 px-2 rounded text-white mb-5"
+            >
+              <p>{taskList.task}</p>
 
-            <button>
-              <BiTrash size={18} color="#fff" />
-            </button>
-          </div>
-        </section>
-      </main>
+              <button>
+                <BiTrash size={18} color="#fff" />
+              </button>
+            </div>
+          </section>
+        </main>
+      ))}
     </div>
   );
 }

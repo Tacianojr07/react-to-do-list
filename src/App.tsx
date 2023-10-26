7de30168e2b880a2969ca4b176e7a6ac43b24662
Tacianojr07 <t.juniior13@gmail.com>
2023-10-26 19:52:55 -0300

    :slipstick: feat: styled completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,28 @@ import { BiTrash } from "react-icons/bi";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>TO-DO NOW</h1>
+    <div className="flex flex-col w-full h-screen items-center ">
+      <header className=" mt-4 mb-11 border-b-2 p-2">
+        <h1 className="font-bold text-4xl text-white mt-5">TO-DO NOW</h1>
       </header>
 
-      <form>
-        <input type="text" required />
-        <button type="submit">Add task</button>
+      <form className="flex items-center w-full max-w-xl justify-center  mb-12 border-b-2 pb-6 ">
+        <input
+          className="w-full bg-slate-200 rounded py-1 px-2 outline-none"
+          type="text"
+          required
+        />
+        <button
+          className=" bg-slate-700 rounded w-36 h-8  font-bold text-white py-1 px-2 "
+          type="submit"
+        >
+          Add task
+        </button>
       </form>
 
-      <main>
-        <section>
-          <div>
+      <main className="flex flex-col w-full items-center">
+        <section className=" flex flex-col w-full items-center justify-center">
+          <div className="flex justify-between w-full max-w-xl bg-slate-950 py-2 px-2 rounded text-white mb-5">
             <p>this a task</p>
 
             <button>

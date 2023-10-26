aa4749f97fdc72a97867975572d9f26a2f4869b4
Tacianojr07 <t.juniior13@gmail.com>
2023-10-26 18:34:45 -0300

    :sparkles: feat: structuring html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { BiTrash } from "react-icons/bi";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <header>
+        <h1>TO-DO NOW</h1>
+      </header>
+
+      <form>
+        <input type="text" required />
+        <button type="submit">Add task</button>
+      </form>
+
+      <main>
+        <section>
+          <div>
+            <p>this a task</p>
+
+            <button>
+              <BiTrash size={18} color="#fff" />
+            </button>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
